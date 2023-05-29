a80e60ad2562679aa7bc7717dd4cfd42c0240a4e
Juan Robi Lara <juanrobilara1@gmail.com>
2023-05-29 15:30:08 -0300

    se contemplaron los 13 casos de prueba a utilizar

---
+++ test/FintechTest.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class FintechTest {
+	
+	Fintech nuevaApp = new Fintech("Bitso");
+
+	@Test
+	public void queSePuedaCrearUnaCuenta() {
+
+	}
+	
+	@Test
+	public void queNoSePuedanCrearCuentasRepetidas() {
+
+	}
+	
+	@Test
+	public void queSePuedaTransferirEnPesos() {
+
+	}
+	
+	@Test
+	public void queSePuedaComprarBitcoinsA_200_Pesos() {
+
+	}
+	
+	@Test
+	public void queSePuedaComprarEthereumA_100_Pesos() {
+
+	}
+	
+	@Test
+	public void queSePuedaComprarDaiA_50_Pesos() {
+
+	}
+	
+	@Test
+	public void queSePuedaTenerDivididoElSaldoEnPesosYEnCripto() {
+		
+
+		
+	}
+	
+	
+	@Test
+	public void queSePuedaRetirarPesos() {
+
+	}
+	
+	
+	@Test
+	public void queSePuedaAceptarUnaSolicitudDeAmistad() {
+
+
+	}
+	
+	@Test
+	public void queSePuedaDenegarUnaSolicitudDeAmistad() {
+
+	}
+	
+	@Test
+	public void queSePuedaRegistrarLasTransaccionesHechas() {	
+
+	        
+		
+	}
+	
+	@Test
+	public void queNoSePuedaRetirarSaldoNegativo() {
+
+	}
+	
+	@Test
+	public void queSePuedanListarLosAmigosDeUnaCuenta() {
+
+	}
+
+	
+
+}
